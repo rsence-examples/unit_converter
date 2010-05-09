98607492a9f3bb8e8b1f62259b2db8ec21392de6
Toni Nurminen <tznurmin@80-248-246-103.cust.suomicom.fi>
2010-05-09 23:30:15 +0300

    License header changed to LGPL

--- unit_converter.rb
+++ unit_converter.rb
@@ -1,10 +1,9 @@
 #--
-##   Riassence Framework
- #   Copyright 2010 Riassence Inc.
+##   Copyright 2010 Riassence Inc.
  #   http://riassence.com/
  #
- #   You should have received a copy of the GNU General Public License along
- #   with this software package. If not, contact licensing@riassence.com
+ #   You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+ #   along with this software package. If not, contact licensing@riassence.com
  ##
  #++
 
